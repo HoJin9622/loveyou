30decd064c8f2e664e9e19ee07186787e9dd8c70
이호진 <kiss0104040@gmail.com>
2022-03-05 13:07:21 +0900

    ✨ feat: add sentry initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Config from 'react-native-config';
+import * as Sentry from '@sentry/react-native';
 import {SafeAreaView} from 'react-native';
 import Hi from '@components/Hi';
 
+Sentry.init({
+  dsn: Config.SENTRY_DSN,
+  integrations: [
+    new Sentry.ReactNativeTracing({tracingOrigins: ['localhost', /^\//]}),
+  ],
+});
+
 const App = () => {
-  console.log(Config.SENTRY_DSN);
   return (
     <SafeAreaView>
       <Hi></Hi>
@@ -12,4 +19,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
