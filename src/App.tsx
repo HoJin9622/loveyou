c7b7335c1c52f66f0ec45820f6ba575c88701aad
이호진 <kiss0104040@gmail.com>
2022-03-05 12:57:13 +0900

    🏗️ build: install react-native-config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Config from 'react-native-config';
 import {SafeAreaView} from 'react-native';
 import Hi from '@components/Hi';
 
 const App = () => {
+  console.log(Config.SENTRY_DSN);
   return (
     <SafeAreaView>
       <Hi></Hi>
