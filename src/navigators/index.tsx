a150288e156feb32bf222c596442597fe10dc7f9
이호진 <kiss0104040@gmail.com>
2022-03-09 22:43:20 +0900

    ✨ feat(screens): add initialRouteName

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
 import Enter from '@screens/Enter';
-import {RootStackParamList} from './navigator';
 import Home from '@screens/Home';
+import {RootStackParamList} from './navigator';
+import {ActivityIndicator} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootStack = () => {
-  return (
-    <Stack.Navigator>
+  const [loading, setLoading] = useState(false);
+  const [dataExist, setDataExist] = useState(false);
+  const {getItem} = useAsyncStorage('profile');
+  useEffect(() => {
+    getProfile();
+  }, []);
+  const getProfile = async () => {
+    setLoading(prev => !prev);
+    const profile = await getItem();
+    if (profile) {
+      setDataExist(true);
+    }
+    setLoading(prev => !prev);
+  };
+  return loading ? (
+    <ActivityIndicator />
+  ) : (
+    <Stack.Navigator initialRouteName={dataExist ? 'Home' : 'Enter'}>
       <Stack.Screen
         name="Enter"
         component={Enter}
