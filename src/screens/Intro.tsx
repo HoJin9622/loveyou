06e859419aef5ee56830538105b09b70772f71d2
이호진 <kiss0104040@gmail.com>
2022-06-14 22:57:33 +0900

    ✨ feat: install expo image picker & pick image

--- src/screens/Intro.tsx
+++ src/screens/Intro.tsx
@@ -11,6 +11,129 @@ import DateTimePicker, {
   DateTimePickerAndroid,
   DateTimePickerEvent,
 } from '@react-native-community/datetimepicker'
+import * as ImagePicker from 'expo-image-picker'
+
+type Props = NativeStackScreenProps<RootStackParamsList, 'Intro'>
+
+const Intro = ({ navigation }: Props) => {
+  const { colors } = useTheme()
+  const [photo, setPhoto] = useState<string | null>(null)
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Text style={{ color: '#fff' }}>완료</Text>,
+    })
+  }, [navigation])
+  const onPhotoBoxClick = async () => {
+    Keyboard.dismiss()
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      base64: true,
+    })
+    if (!result.cancelled && result.base64) {
+      setPhoto(`data:image/jpeg;base64,${result.base64}`)
+    }
+  }
+
+  // const [date, setDate] = useState(new Date(1598051730000))
+
+  // const onChange = (event: DateTimePickerEvent, selectedDate: Date) => {
+  //   const currentDate = selectedDate
+  //   setDate(currentDate)
+  // }
+
+  // const showMode = (currentMode: string) => {
+  //   DateTimePickerAndroid.open({
+  //     value: date,
+  //     onChange,
+  //     mode: currentMode,
+  //     is24Hour: true,
+  //   })
+  // }
+
+  // const showDatepicker = () => {
+  //   showMode('date')
+  // }
+
+  return (
+    <DismissKeyboard>
+      <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={90}>
+        <Container>
+          <StatusBar style='light' />
+          <PhotoBox onPress={onPhotoBoxClick}>
+            {photo ? (
+              <Photo source={{ uri: photo }} />
+            ) : (
+              <>
+                <Entypo name='plus' size={44} color={colors.black900} />
+                <Text
+                  style={{
+                    color: colors.black500,
+                    fontSize: 16,
+                    lineHeight: 19,
+                    marginTop: 47,
+                  }}
+                >
+                  여기를 터치해서
+                </Text>
+                <Text
+                  style={{
+                    color: colors.black500,
+                    fontSize: 16,
+                    lineHeight: 19,
+                  }}
+                >
+                  원하는 사진을 선택해주세요.
+                </Text>
+              </>
+            )}
+          </PhotoBox>
+          <Input
+            placeholder='이름 or 애칭'
+            placeholderTextColor={colors.black0}
+          />
+          <RowLayout>
+            <Feather
+              name='calendar'
+              size={24}
+              color={colors.black0}
+              style={{ marginRight: 8 }}
+            />
+            <Text
+              // onPress={showDatepicker}
+              style={{ color: colors.black0, fontSize: 16, lineHeight: 19 }}
+            >
+              생일
+            </Text>
+            <Feather
+              name='calendar'
+              size={24}
+              color={colors.black0}
+              style={{ marginRight: 8, marginLeft: 24 }}
+            />
+            <Text
+              style={{ color: colors.black0, fontSize: 16, lineHeight: 19 }}
+            >
+              사귀기 시작한 날
+            </Text>
+          </RowLayout>
+          {/* {true && (
+            <DateTimePicker
+              style={{ width: '100%' }}
+              testID='dateTimePicker'
+              value={date}
+              mode='date'
+              onChange={onChange}
+              display='spinner'
+            />
+          )} */}
+        </Container>
+      </KeyboardAvoidingView>
+    </DismissKeyboard>
+  )
+}
+
+export default Intro
 
 const Container = styled.View`
   flex: 1;
@@ -28,6 +151,11 @@ const PhotoBox = styled.Pressable`
   justify-content: center;
   align-items: center;
 `
+const Photo = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 14px;
+`
 const Input = styled.TextInput`
   font-size: 32px;
   line-height: 38px;
@@ -37,106 +165,3 @@ const Input = styled.TextInput`
 const KeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex: 1;
 `
-
-type Props = NativeStackScreenProps<RootStackParamsList, 'Intro'>
-
-const Intro = ({ navigation }: Props) => {
-  const { colors } = useTheme()
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => <Text style={{ color: '#fff' }}>완료</Text>,
-    })
-  }, [navigation])
-  const onPhotoBoxClick = () => {
-    Keyboard.dismiss()
-  }
-
-  const [date, setDate] = useState(new Date(1598051730000))
-
-  const onChange = (event: DateTimePickerEvent, selectedDate: Date) => {
-    const currentDate = selectedDate
-    setDate(currentDate)
-  }
-
-  const showMode = (currentMode: string) => {
-    DateTimePickerAndroid.open({
-      value: date,
-      onChange,
-      mode: currentMode,
-      is24Hour: true,
-    })
-  }
-
-  const showDatepicker = () => {
-    showMode('date')
-  }
-
-  return (
-    <DismissKeyboard>
-      <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={90}>
-        <Container>
-          <StatusBar style='light' />
-          <PhotoBox onPress={onPhotoBoxClick}>
-            <Entypo name='plus' size={44} color={colors.black900} />
-            <Text
-              style={{
-                color: colors.black500,
-                fontSize: 16,
-                lineHeight: 19,
-                marginTop: 47,
-              }}
-            >
-              여기를 터치해서
-            </Text>
-            <Text
-              style={{ color: colors.black500, fontSize: 16, lineHeight: 19 }}
-            >
-              원하는 사진을 선택해주세요.
-            </Text>
-          </PhotoBox>
-          <Input
-            placeholder='이름 or 애칭'
-            placeholderTextColor={colors.black0}
-          />
-          <RowLayout>
-            <Feather
-              name='calendar'
-              size={24}
-              color={colors.black0}
-              style={{ marginRight: 8 }}
-            />
-            <Text
-              onPress={showDatepicker}
-              style={{ color: colors.black0, fontSize: 16, lineHeight: 19 }}
-            >
-              생일
-            </Text>
-            <Feather
-              name='calendar'
-              size={24}
-              color={colors.black0}
-              style={{ marginRight: 8, marginLeft: 24 }}
-            />
-            <Text
-              style={{ color: colors.black0, fontSize: 16, lineHeight: 19 }}
-            >
-              사귀기 시작한 날
-            </Text>
-          </RowLayout>
-          {true && (
-            <DateTimePicker
-              style={{ width: '100%' }}
-              testID='dateTimePicker'
-              value={date}
-              mode='date'
-              onChange={onChange}
-              display='spinner'
-            />
-          )}
-        </Container>
-      </KeyboardAvoidingView>
-    </DismissKeyboard>
-  )
-}
-
-export default Intro
