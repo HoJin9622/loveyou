50c4e36ad51ea1ecf738fba11fed89f8ad17ba05
이호진 <kiss0104040@gmail.com>
2022-06-29 23:44:43 +0900

    🐛 fix: comingup 날짜 오류 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -20,18 +20,19 @@ export const getComingDate = (firstDay: Date): [dayjs.Dayjs, number] => {
 export const getAnniversaries = (firstDay: Date, maxLength: number) => {
   const now = dayjs(new Date())
   let anniversaries = []
-  const day50 = dayjs(firstDay).add(50, 'd')
-  anniversaries.push({ date: day50, comingUp: day50.isBefore(now) })
+  const day50 = dayjs(firstDay).add(49, 'd')
+  let setComingUp = day50.isAfter(now)
+  anniversaries.push({ date: day50, comingUp: setComingUp })
   for (let i = 1; i < maxLength; i++) {
-    const anniversary = dayjs(firstDay).add(i * 100, 'd')
-    const comingUp = dayjs(anniversary).isBefore(now)
-    if (comingUp) {
-      anniversaries = anniversaries.map((anniversary) => ({
-        date: anniversary.date,
-        comingUp: false,
-      }))
+    const anniversary = dayjs(firstDay)
+      .add(i * 100, 'd')
+      .add(-1, 'd')
+    if (!setComingUp) {
+      setComingUp = dayjs(anniversary).isAfter(now)
+      anniversaries.push({ date: anniversary, comingUp: setComingUp })
+    } else {
+      anniversaries.push({ date: anniversary, comingUp: false })
     }
-    anniversaries.push({ date: anniversary, comingUp })
   }
   return anniversaries
 }
